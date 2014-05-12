bb538ebe3e787129c362b7699a01856551e39761
mick-roper <fabzybaby@live.co.uk>
2014-05-12 13:16:35 +0100

    Base select objects added.

---
+++ SqlFactory/SqlFactory.Tests/SelectTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlFactory.Tests
+{
+    [TestClass]
+    public class SelectTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SqlFactory/SqlFactory/From.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlFactory
+{
+    /// <summary>
+    /// Creates the FROM section of a sql statement.
+    /// </summary>
+    public class From
+    {
+        public string Table { get; private set; }
+        public Where Where { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="From"/> class.
+        /// </summary>
+        /// <param name="schema">The schema.</param>
+        /// <param name="table">The table.</param>
+        public From(string schema, string table)
+        {
+            Table = string.Format("{0}.{1}", schema, table);
+        }
+
+        public Where Where(string condition)
+        {
+            Where = new Where(condition);
+            return Where;
+        }
+
+        public override string ToString()
+        {
+            return Table;
+        }
+    }
+}
